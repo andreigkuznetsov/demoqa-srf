3db211f30f2b96d6736faf56cc4a4f865b22ed59
andreikuz <andreikuz@yahoo.com>
2021-10-26 23:08:36 +0300

    this is new srf file

--- src/test/java/yahoo/andreikuzn/StudentRegistrationForm.java
+++ src/test/java/yahoo/andreikuzn/StudentRegistrationForm.java
@@ -4,10 +4,11 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class StudentRegistrationForm {
 
@@ -24,8 +25,27 @@ public class StudentRegistrationForm {
         $("#userEmail").setValue("andrey@kuznetsov.com");
         $(byText("Male")).click();
         $("#userNumber").setValue("8950789456");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("10");
+        $(".react-datepicker__year-select").selectOptionByValue("1976");
+        $$(".react-datepicker__day").find(text("13")).click();
+        $("#subjectsInput").click();
+        $("#subjectsInput").sendKeys("c");
+        $(byText("Economics")).click();
+        $(byText("Sports")).click();
+        File image = new File("src/test/resources/pic.jpg");
+        $("#uploadPicture").uploadFile(image);
+        $("#currentAddress").setValue("Russia");
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $(byText("Delhi")).click();
+        $("#submit").scrollIntoView(true);
+        $("#submit").click();
 
-        //$("#submit").click();
+
+
+                //$("#submit").click();
 
         //$("#output #name").shouldHave(text("Some name"));
         //$("#output").$("#email").shouldHave(text("aaa@aa.aa"));
