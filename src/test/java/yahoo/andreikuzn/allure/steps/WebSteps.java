8992fcad8fdc649a058f4e6a88d1700dae4f9ffc
andreikuz <andreikuz@yahoo.com>
2021-11-10 09:44:57 +0300

    this is Allure homework

--- src/test/java/yahoo/andreikuzn/allure/steps/WebSteps.java
+++ src/test/java/yahoo/andreikuzn/allure/steps/WebSteps.java
@@ -1,6 +1,5 @@
 package yahoo.andreikuzn.allure.steps;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
@@ -9,10 +8,8 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.partialLinkText;
 
 public class WebSteps {
