81721a7753c95606784d0cf0f4a7c99b22628f19
andreikuz <andreikuz@yahoo.com>
2021-11-13 13:06:54 +0300

    this is files homework

--- src/test/java/yahoo/andreikuzn/files/FilesTests.java
+++ src/test/java/yahoo/andreikuzn/files/FilesTests.java
@@ -1,20 +1,71 @@
 package yahoo.andreikuzn.files;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class FilesTests {
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static yahoo.andreikuzn.files.TestData.*;
+
+public class FilesTests extends TestBase {
 
     @Test
     @DisplayName("Загрузка файлов в файловый обменник")
     void uploadFileInToFilesStorage() {
-        open("http://file.karelia.ru/");
-        $("#file_0").uploadFromClasspath("pic.jpg", "pic1.jpg");
+        open(UPLOADFILESLINK);
+        $("#file_0").uploadFromClasspath(FILETOUPLOAD1, FILETOUPLOAD2);
         $("#file_submit").click();
-        $("#fileQueue").shouldHave(text("pic.jpg"));
-        $("#fileQueue").shouldHave(text("pic1.jpg"));
+        $("#fileQueue").shouldHave(text(FILETOUPLOAD1));
+        $("#fileQueue").shouldHave(text(FILETOUPLOAD2));
     }
+
+    @Test
+    @DisplayName("Работа с .txt файлом (скачивание и поиск по содержимому)")
+    void downloadTextFileAndSearchInsideIt() throws IOException {
+        open(DOWNLOADTXTFILELINK);
+        $(".CookieAcceptance-accept").click();
+        File download = $("#downloadButton").download();
+        String fileContent = IOUtils.toString(new FileReader(download));
+        assertTrue(fileContent.contains(TXTFILECONTAINSTEXT));
+    }
+
+    @Test
+    @DisplayName("Работа с .pdf файлом (скачивание и поиск по содержимому)")
+    void downloadPdfFileAndSearchInsideIt() throws IOException {
+        open(DOWNLOADPDFFILELINK);
+        //$(".CookieAcceptance-accept").click(); необходимо раскомментировать, если тест запускается отдельно
+        File pdf = $("#downloadButton").download();
+        PDF parsedPdf = new PDF(pdf);
+        Assertions.assertEquals(false, parsedPdf.encrypted);
+    }
+
+    @Test
+    @DisplayName("Работа с .xls файлом (скачивание и поиск по содержимому)")
+    void downloadXlsFileAndSearchInsideIt() throws IOException {
+        open(DOWNLOADXLSFILELINK);
+        //$(".CookieAcceptance-accept").click(); необходимо раскомментировать, если тест запускается отдельно
+        File file = $("#downloadButton").download();
+
+        XLS parsedXls = new XLS(file);
+        boolean checkPassed = parsedXls.excel
+                .getSheetAt(0)
+                .getRow(3)
+                .getCell(2)
+                .getStringCellValue()
+                .contains(XLSFILECONTAINSTEXT);
+
+        assertTrue(checkPassed);
+    }
+
 }
---
+++ src/test/java/yahoo/andreikuzn/files/TestData.java
@@ -0,0 +1,15 @@
+package yahoo.andreikuzn.files;
+
+import java.io.File;
+
+public class TestData {
+
+    public static String UPLOADFILESLINK = "http://file.karelia.ru/";
+    public static String DOWNLOADTXTFILELINK = "https://www.mediafire.com/file/ljpetr163biym4y/doc.txt/file";
+    public static String DOWNLOADPDFFILELINK = "https://www.mediafire.com/file/zu932e8fbbrk0tf/doc1.pdf/file";
+    public static String DOWNLOADXLSFILELINK = "https://www.mediafire.com/file/yqzr13lmgilg8we/doc2.xls/file";
+    public static String FILETOUPLOAD1 = "pic.jpg";
+    public static String FILETOUPLOAD2 = "pic1.jpg";
+    public static String TXTFILECONTAINSTEXT = "В Java есть 4 вида циклов: while, for, for-each и do-while.";
+    public static String XLSFILECONTAINSTEXT = "392699006";
+}
